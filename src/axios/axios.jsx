5750e8464a2b0447d72a76d2816703da45b54b78
Yogeeshap <anusharun88@gmail.com>
2021-09-04 20:59:30 +0530

    changes done

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:8000/api'
+const baseURL = process.env.REACT_APP_BASE_URL
 const axiosInstance = axios.create({
     baseURL:baseURL,
     timeout:5000
